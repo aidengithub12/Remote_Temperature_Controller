46c159902117a2e491856e9ca40e1bf3927408e8
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2024-09-26 20:28:01 -0500

    Create CarServo.ino

---
+++ CarServo/CarServo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+const gearRatio = 2;
+//2:1 ratio
+
+int Servopin = 33;
+Servo servo;
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(Servopin);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
